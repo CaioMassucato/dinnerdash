4e96c5f0c092afe1f535d8d13ad15ec24fc4668c
dieg0d <diegodi05@hotmail.com>
2019-07-07 22:42:50 -0300

    first commit

--- dinnerdash_front/src/pages/Home/index.js
+++ dinnerdash_front/src/pages/Home/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./style.css";
-
+import Banner from "../../components/Banner";
 export default class Home extends Component {
   render() {
     return (
-      <div className="container">
-        <h1 className="vermelho">Home</h1>
-        <button className="btn btn-success">ola</button>
+      <div>
+        <Banner />
+        <div className="container">
+          <h1 className="vermelho">Home</h1>
+          <button className="btn btn-success">ola</button>
+        </div>
       </div>
     );
   }

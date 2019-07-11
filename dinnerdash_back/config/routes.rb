8ea81281f5f104c2388effd828e3e3d4379812fc
Raphael Muller <raphaelplmuller@icloud.com>
2019-07-11 19:37:14 -0300

    Adicionado model, CRUD e rotas para categorias de refeições

--- dinnerdash_back/config/routes.rb
+++ dinnerdash_back/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/meal_categories', to: 'meal_categories#index'
+  get '/meal_categories/:id', to: 'meal_categories#show'
+  post '/meal_categories', to: 'meal_categories#create'
+  put '/meal_categories/:id', to: 'meal_categories#update'
+  delete '/meal_categories/:id', to: 'meal_categories#destroy'
+
 end

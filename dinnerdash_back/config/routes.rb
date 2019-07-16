85ff200b6bf18ab7d1fa2eb62080d7fbc408b243
mekamdan <m.alencar.v@outlook.com>
2019-07-16 16:52:35 -0300

    rota foi endireitada

--- dinnerdash_back/config/routes.rb
+++ dinnerdash_back/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'meal/create'
-  get 'meal/show'
-  get 'meal/index'
-  get 'meal/update'
-  get 'meal/destroy'
+  get 'meal/create', to: 'meal#create'
+  get 'meal/show' , to: 'meal#create'
+  get 'meal/index', to: 'meal#create'
+  get 'meal/update', to: 'meal#create'
+  get 'meal/destroy', to: 'meal#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

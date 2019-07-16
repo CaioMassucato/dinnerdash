c0a200c700b6755770c4ed559149f4ddfac6ba76
gabrielgrc <gabriel.bsb98@gmail.com>
2019-07-15 21:59:10 -0300

    CRUD, model e rotas para Pedido

--- dinnerdash_back/config/routes.rb
+++ dinnerdash_back/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#Rotas para Orders
+ get '/orders/index', to: 'orders#index'
+ get '/orders/show/:id', to: 'orders#show'
+ post '/orders/create', to: 'orders#create'
+ put '/orders/update/:id', to: 'orders#update'
+ delete '/orders/destroy/:id', to: 'orders#destroy'
 end

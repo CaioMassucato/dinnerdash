30fd01bbc98841c9c58363b6d21f0f3d1be6e869
gabrielgrc <gabriel.bsb98@gmail.com>
2019-07-19 16:25:53 -0300

    bannerdinnerdash

--- dinnerdash_front/src/components/Banner/index.js
+++ dinnerdash_front/src/components/Banner/index.js
@@ -4,7 +4,27 @@ import "./style.css";
 export default class Banner extends Component {
   render() {
     return (
-      <div className="banner">
+      <div className="header-descricao">
+        <div className="header" />
+
+        <div className="produto">
+          <span>dinnerdash</span>
+        </div>
+
+        <div className="cadastro">
+          <span>cadastro</span>
+        </div>
+
+        <div className="entrar">
+          <span>entrar</span>
+        </div>
+
+        <div id="descricao">
+          <span>Monte o seu prato</span>
+          <h2>
+            Escolha sua salada, molho, prato principal, guarnição e bebida
+          </h2>
+        </div>
       </div>
     );
   }

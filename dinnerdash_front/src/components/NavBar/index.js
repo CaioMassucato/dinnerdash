3371f794d471481f517109219ed98573101fe29e
gabrielgrc <gabriel.bsb98@gmail.com>
2019-07-20 13:08:19 -0300

    Navbar do Backoffice

---
+++ dinnerdash_front/src/components/NavBar/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./style.css";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+        <body>
+        <div class="alert alert-info" role="alert">
+                <div id = "espacamento">Pedidos Usuários Refeições <div id = "categorias">Categorias de refeição</div></div>     
+              </div>
+</body>
+    );
+  }
+}
+

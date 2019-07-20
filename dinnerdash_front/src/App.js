ab4a704b3ac216f84bc8a171d6d3eae14723f7df
CaioMassucato <caiobernardon@gmail.com>
2019-07-20 18:13:52 -0300

    Último pedido imgs estáticas

--- dinnerdash_front/src/App.js
+++ dinnerdash_front/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import BackLast from "./pages/BackLast";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/last" component={BackLast} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Router>
--- dinnerdash_front/src/api.js
+++ dinnerdash_front/src/api.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL: 'http://localhost:3000' })
-
+export const api = axios.create({ baseURL: "http://localhost:3000" });

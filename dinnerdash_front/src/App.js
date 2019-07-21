5c1628532c2e12cae2a2cf0b2a1c439b861d0b80
CaioMassucato <caiobernardon@gmail.com>
2019-07-20 22:13:15 -0300

    Edit user quase lá

--- dinnerdash_front/src/App.js
+++ dinnerdash_front/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import FormEditUser from "./components/FormEditUser";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/edituser" component={FormEditUser} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Router>
--- dinnerdash_front/src/api.js
+++ dinnerdash_front/src/api.js
@@ -1,4 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL: 'http://localhost:3000' })
+export const api = axios.create({ baseURL: "http://localhost:3000" });
 
+export const update = (email, password, admin) => {
+  return api.put("/users", { email, password, admin });
+};

765c29decf74a6f27b088a5e6b361e7eedc6a2f8
gabrielgrc <gabriel.bsb98@gmail.com>
2019-07-22 12:14:55 -0300

    pagina de perfil do usuario

---
+++ dinnerdash_front/src/pages/PerfildoUsuario/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./style.css";
+import FormEdicaoPerfil from "../../components/FormEdicaoPerfil"
+​
+export default class EditUserPage extends Component {
+  render() {
+    return (
+      <div >
+	  	<FormEdicaoPerfil />	
+      </div>
+    );
+  }
+}
